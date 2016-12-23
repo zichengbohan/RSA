ae148eaaa9af751a7162692c57247f1804685982
胥佰淼 <xbmld1@gmail.com>
2016-12-23 17:07:39 +0800

    Initial Commit

---
+++ RSApem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RSApem
+//
+//  Created by 胥佰淼 on 2016/12/23.
+//  Copyright © 2016年 胥佰淼. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RSApem/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RSApem
+//
+//  Created by 胥佰淼 on 2016/12/23.
+//  Copyright © 2016年 胥佰淼. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
